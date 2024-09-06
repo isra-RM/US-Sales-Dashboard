35b3a933acac67e6e111c9bb114a75d32cbfc86f
Israel Reyes-Molina <108302899+isra-RM@users.noreply.github.com>
2024-09-06 10:44:32 -0500

    initializing

---
+++ app.py
@@ -0,0 +1,230 @@
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import altair as alt
+import folium
+from folium.plugins import HeatMap
+from shiny import reactive
+from shiny.express import render,input,ui
+from shinywidgets import render_plotly,render_altair,render_widget
+import calendar
+
+#STYLING
+
+ui.tags.style(
+    """
+    body {
+        background-color: #5DADE2;
+    }
+    
+    .modebar-container{
+        display: none;
+    }
+    
+    
+    h2 {
+        text-align : center;
+        color: white;
+        padding : 10px 20px;
+        font-weight : bold;
+        font-size : 50px ;
+    }
+    
+    summary {
+        display: none;
+    }
+    """
+)
+
+
+#FUNCTION TO APPLY STYLE TO PLOTLY GRAPHS
+
+def style_plotly_chart(fig,yaxis_title):
+    fig.update_layout(
+        xaxis_title = '',
+        yaxis_title = yaxis_title,
+        plot_bgcolor = 'white',
+        showlegend = False,
+        coloraxis_showscale = False,
+        font = {'family':'Arial','size':12}
+    )
+    fig.update_xaxes(
+        showgrid = False
+    )
+    fig.update_yaxes(
+        showgrid = False
+    )
+    return fig
+
+
+
+ui.page_opts(title='Sales Dashboard for US Cities',fillable=False)
+
+#READING DATA AND CALCULATING NEW USEFUL COLUMNS 
+
+@reactive.calc
+def dat():
+    infile = Path(__file__).parent / "data/sales.csv"
+    df = pd.read_csv(infile)
+    df['order_date'] = pd.to_datetime(df['order_date'])
+    df['month'] = df['order_date'].dt.month_name()
+    df['hour'] = df['order_date'].dt.hour
+    df['day'] = df['order_date'].dt.day_name()
+    df['value'] = df['quantity_ordered']*df['price_each']
+    return df
+   
+#GENERAL LAYOUT OF THE DASHBOARD
+
+with ui.card():
+    ui.card_header("Sales by City in 2023")
+    
+    with ui.layout_sidebar(): 
+        with ui.sidebar(bg="#f8f8f8",open='open'):  
+            ui.input_selectize(
+                "city",
+                "Select a City:",
+                [
+                'Dallas (TX)',
+                'Boston (MA)', 
+                'Los Angeles (CA)',
+                'San Francisco (CA)',
+                'Seattle (WA)',
+                'Atlanta (GA)',
+                'New York City (NY)',
+                'Portland (OR)',
+                'Austin (TX)',
+                'Portland (ME)'],
+                multiple=False,
+                selected = "Boston (MA)"
+            )
+        @render_altair
+        def sales_over_time():
+            df = dat()
+            sales = df.groupby(['city','month'])['quantity_ordered'].sum().reset_index()
+            sales_by_city = sales[sales['city'] == input.city()]
+            month_order = calendar.month_name[1:]
+            font_props = alt.Axis(labelFont="Arial",titleFont="Arial",labelFontSize=12,titleFontSize=15,tickSize=0,labelAngle=0)
+            chart = alt.Chart(sales_by_city).mark_bar().encode(
+              x = alt.X('month',sort=month_order,title='Month',axis=font_props),
+              y = alt.Y('quantity_ordered',title='Quantity Ordered',axis=font_props),
+              tooltip=['month','quantity_ordered']  
+            ).properties(
+                title = alt.Title(f"Sales over time -- {input.city()}")
+            ).configure_axis(
+                grid=False               
+            ).configure_title(
+                font = 'Arial',
+                fontSize=18
+            )
+            
+            return chart
+
+with ui.layout_column_wrap(width=1/2):
+    with ui.navset_card_underline( id='tab',footer=ui.input_numeric("n","Number of items",5,min=0,max=20)):
+            with ui.nav_panel("Top Sellers"):
+                
+                @render_plotly
+                def plot_top_sellers():
+                    df = dat()
+                    top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.n()).reset_index()
+                    fig = px.bar(top_sales,
+                                 x = 'product',
+                                 y='quantity_ordered',
+                                 color='quantity_ordered',
+                                 color_continuous_scale='Blues')
+                    fig = style_plotly_chart(fig,'Quantity Order')
+                    return fig
+                
+            with ui.nav_panel("Top Sellers Value ($)"):
+                
+                @render_plotly
+                def plot_top_sellers_value():
+                    df = dat()
+                    top_sales = df.groupby('product')['value'].sum().nlargest(input.n()).reset_index()
+                    fig = px.bar(top_sales,
+                                 x = 'product',
+                                 y='value',
+                                 color='value',
+                                 color_continuous_scale='Blues')
+                    fig = style_plotly_chart(fig,'Total Sales ($)')
+                    return fig 
+                
+            with ui.nav_panel("Lowest Sellers"):
+                
+                @render_plotly
+                def plot_lowest_sellers():
+                    df = dat()
+                    top_sales = df.groupby('product')['quantity_ordered'].sum().nsmallest(input.n()).reset_index()
+                    fig = px.bar(top_sales,x = 'product',
+                                 y='quantity_ordered',
+                                 color='quantity_ordered',
+                                 color_continuous_scale='Reds')
+                    fig = style_plotly_chart(fig,'Quantity Order')
+                    return fig 
+                
+            with ui.nav_panel("Lowest Sellers Value ($)"):
+                
+                @render_plotly
+                def plot_lowest_sellers_value():
+                    df = dat()
+                    top_sales = df.groupby('product')['value'].sum().nsmallest(input.n()).reset_index()
+                    fig = px.bar(top_sales,
+                                 x = 'product',
+                                 y='value',
+                                 color='value',
+                                 color_continuous_scale='Reds')
+                    fig = style_plotly_chart(fig,'Total Sales ($)')
+                    return fig 
+    with ui.card():
+        ui.card_header("Sales by Time of Day Heatmap")
+        with ui.card_footer():
+            ui.input_selectize(
+                "day",
+                "Select a Day:",
+                [
+                'Monday',
+                'Tuesday', 
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday'],
+                multiple=False,
+                selected = "Monday"
+            )
+
+        
+        @render.plot
+        def plot_sales_by_time():
+            df = dat()
+            df_day = df[df['day'] == input.day()]
+            sales_by_hour = df_day['hour'].value_counts().reindex(np.arange(0,24),fill_value=0)
+            heatmap_data = sales_by_hour.values.reshape(24,1)
+            sns.heatmap(heatmap_data,
+                        annot=True,
+                        fmt='d',
+                        cmap='coolwarm',
+                        cbar=False,
+                        xticklabels=[],
+                        yticklabels=[f"{i}:00" for i in range(24)])
+            plt.xlabel("Order Count")
+            plt.ylabel("Hour of Day")
+             
+        
+with ui.card():
+    ui.card_header("Sales by Location")
+    @render.ui
+    def plot_us_heatmap():
+        df = dat()
+        
+        heatmap_data = df[['lat','long','quantity_ordered']].values
+        map = folium.Map(location=[37.09,-95.71],zoom_start=4.3)
+        HeatMap(heatmap_data).add_to(map)
+        
+        return map
+        
+    
+    
